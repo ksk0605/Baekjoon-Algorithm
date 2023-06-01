f88fa53540c98fc0589fc8fd4218dce1d838fdae
SUNGKYUM KIM <76910498+ksk0605@users.noreply.github.com>
2023-06-01 19:21:57 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 71.02 ms, Memory: 26.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=python3) 
 
 ### 성능 요약
 
-메모리: 33.9 MB, 시간: 45.82 ms
+메모리: 26.3 MB, 시간: 71.02 ms
 
 ### 구분
 
--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,15 +1,22 @@
 def solution(participant, completion):
-    dic = {}
-    for i in participant: 
-        if i in dic: 
-            dic[i] += 1
-        else:
-            dic[i] = 1
+    participant.sort()
+    completion.sort()
+    
+    for i in range(len(completion)): 
+        if participant[i] != completion[i]:
+            return participant[i]
+    return participant[-1]
+#     dic = {}
+#     for i in participant: 
+#         if i in dic: 
+#             dic[i] += 1
+#         else:
+#             dic[i] = 1
 
     
-    for i in completion: 
-        dic[i] -= 1
+#     for i in completion: 
+#         dic[i] -= 1
     
-    for i in dic: 
-        if dic[i] > 0: 
-            return i
+#     for i in dic: 
+#         if dic[i] > 0: 
+#             return i
