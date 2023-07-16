4c7bba011bb41bda061052b3807d295d3f45e3e7
SUNGKYUM KIM <76910498+ksk0605@users.noreply.github.com>
2023-07-17 02:59:37 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 10.79 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        String answer = "";
+        for (int i = 0; i < my_string.length(); i++) { 
+            if ( s <= i && i < s + overwrite_string.length()) { 
+                answer += overwrite_string.charAt(i-s);
+            } else { 
+                answer += my_string.charAt(i);
+            }
+        }
+        return answer;
+    }
+}
